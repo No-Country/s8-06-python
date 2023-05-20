558e98ea11169503213e19efa24a33a09fc28d1e
Rodrigo Maffei <rodrigoa.maffei@gmail.com>
2023-05-20 00:01:21 -0300

    navbar inicial

--- front/nutriplan/src/app/app.component.ts
+++ front/nutriplan/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faHand } from '@fortawesome/free-regular-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'nutriplan';
+  fasHand = faHand;
 }
--- front/nutriplan/src/app/app.module.ts
+++ front/nutriplan/src/app/app.module.ts
@@ -5,6 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
+import { NgIconsModule } from '@ng-icons/core';
+import { matRamenDiningOutline }from '@ng-icons/material-icons/outline'
+import { heroHome, heroBars4, heroMagnifyingGlass } from '@ng-icons/heroicons/outline';
+
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +16,8 @@ import { NoopAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NoopAnimationsModule
+    NoopAnimationsModule,
+    NgIconsModule.withIcons({ heroHome, heroBars4, heroMagnifyingGlass, matRamenDiningOutline }),
   ],
   providers: [],
   bootstrap: [AppComponent]
