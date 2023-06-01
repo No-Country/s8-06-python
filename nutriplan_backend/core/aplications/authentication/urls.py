3263038baf376d56f09abf5c4aada5a17bf07357
Doliver Gonzalez <doliverpoleo15@gmail.com>
2023-06-01 13:33:10 -0500

    modelo Profesional y vista de recetas html de prueba

--- nutriplan_backend/core/aplications/authentication/urls.py
+++ nutriplan_backend/core/aplications/authentication/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('login/', LoginView.as_view()),
     path('signup/', SignUpView.as_view()),
     path('logout/',LogoutView.as_view()),
+    path('', buscar_recetas, name='buscar_recetas'),
 ]
--- nutriplan_backend/core/aplications/authentication/views.py
+++ nutriplan_backend/core/aplications/authentication/views.py
@@ -8,8 +8,10 @@ from .messages.responses_ok import LOGIN_OK, SIGNUP_OK
 from .messages.responses_error import LOGIN_CREDENTIALS_REQUIRED_ERROR, LOGIN_CREDENTIALS_ERROR
 from .models import User
 from rest_framework.authtoken.models import Token
+import requests
+from django.shortcuts import render
 
-# Create your views here.
+# LOGIN Y REGISTRO
 class LoginView(generics.GenericAPIView):
     def get(self, request):
         data_response = {"msg": "Método GET no permitido"}
@@ -81,3 +83,38 @@ class LogoutView(APIView):
             data = {'code': 401, 'msg': 'Credenciales incorrectas'}
             code = status.HTTP_401_UNAUTHORIZED
         return Response(data, status = code)
+    
+# BUSQUEDA DE RECETAS
+
+def buscar_recetas(request):
+    if request.method == 'GET':
+        query = request.GET.get('q', '') 
+        app_id = '2a1df3d6'  
+        api_key = '44a9edf235f4f291e95637636455047b'  
+
+        url = f'https://api.edamam.com/search?q={query}&app_id={app_id}&app_key={api_key}'
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            recetas = response.json()['hits']
+
+            # Obtener información nutricional de cada receta
+            for receta in recetas:
+                recipe_uri = receta['recipe']['uri']
+                nutri_app_id = '9e5b7d4c'
+                nutri_api_key = '61a30468a7d6ac8b225c357739a9f7df'
+                nutricional_url = f'https://api.edamam.com/api/nutrition-details?app_id={nutri_app_id}&app_key={nutri_api_key}'
+                payload = {'title': receta['recipe']['label'], 'ingr': receta['recipe']['ingredientLines'], 'uri': recipe_uri}
+                nutricional_response = requests.post(nutricional_url, json=payload)
+                if nutricional_response.status_code == 200:
+                    receta['recipe']['nutritional_info'] = nutricional_response.json()
+            
+            recetas.sort(key=lambda x: x['recipe']['label'])
+
+            limited_recetas = recetas[:5]  # Limitar a 5 resultados
+
+            print(limited_recetas)
+            
+            return render(request, 'buscar_recetas.html', {'recetas': limited_recetas})
+
+    return render(request, 'buscar_recetas.html')
