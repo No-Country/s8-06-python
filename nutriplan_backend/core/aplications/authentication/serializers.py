8e9d0bb41882a4615af04033294ff9844ea58473
gregjose25131 <gregjose2513@gmail.com>
2023-06-05 22:15:48 -0400

    Login detalle

--- nutriplan_backend/core/aplications/authentication/serializers.py
+++ nutriplan_backend/core/aplications/authentication/serializers.py
@@ -10,7 +10,7 @@ class UserSerializer(serializers.ModelSerializer):
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'email', 'password', 'first_name', 'last_name')
+        fields = ('id','username', 'email', 'password', 'first_name', 'last_name')
 
     def create(self, validated_data):
         user = User.objects.create_user(
